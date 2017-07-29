3e17cb8ced2b72c768055602c955a967e3d58467
Qingbo Zhou <zhou.qingbo@icloud.com>
2017-07-29 22:09:07 +0800

    create table, using SQL directly.

---
+++ db/certs.sql
@@ -0,0 +1,21 @@
+create table certs (
+  id serial primary key,
+  owner_email varchar(255),
+  imported_at timestamp,
+  subject text,
+  issuer text,
+  serial_number varchar(255),
+  sans text,
+  not_before timestamp,
+  not_after timestamp,
+  sigalg varchar(255),
+  authority_key_id varchar(255),
+  subject_key_id varchar(255),
+  pem text not null,
+  key text,
+  key_receipients text
+);
+
+create index index_certs_owner_email on certs (owner_email);
+create index index_certs_not_after on certs (not_after);
+create index index_certs_authority_key_id on certs (authority_key_id);
