2882734acc48ab9238996eff6893d99fa4c939f6
Qingbo Zhou <zhou.qingbo@icloud.com>
2017-07-30 17:38:02 +0800

    comment on hack

--- app/models/concerns/foreman_cfssl/cert.rb
+++ app/models/concerns/foreman_cfssl/cert.rb
@@ -1,8 +1,6 @@
 module ForemanCfssl
   class Cert < ActiveRecord::Base
-    #attr_accessor :owner_email, :imported_at, :profile, :subject,
-    #  :issuer, :serial_number, :sans, :not_before, :not_after, :sigalg,
-    #  :authority_key_id, :subject_key_id, :pem, :key
+    # quick hack to ease form submission
     attr_accessor :common_name, :hosts
 
     def subject_info
