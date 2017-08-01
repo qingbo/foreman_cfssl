42ae73ea7f9b6eb23ade0716c99585ea0859e505
Qingbo Zhou <zhou.qingbo@icloud.com>
2017-08-01 22:57:10 +0800

    more information on forms

--- app/controllers/foreman_cfssl/certs_controller.rb
+++ app/controllers/foreman_cfssl/certs_controller.rb
@@ -9,11 +9,19 @@ module ForemanCfssl
 
     def import
       @cert = Cert.new
+      @allow_key_import = ['allow', 'true'].include?(SETTINGS[:foreman_cfssl][:private_key_import])
     end
 
     def import_save
       @cert = Cert.new(params[:foreman_cfssl_cert].permit(:owner_email, :pem, :key))
 
+      allow_key_import = ['allow', 'true'].include?(SETTINGS[:foreman_cfssl][:private_key_import])
+      if @cert.key.include?('PRIVATE') && !allow_key_import
+        @cert.errors.add(:key, "Don't upload an unencrypted private key. Consider encrypting it.")
+        render 'import' and return
+      end
+
+      @cert.user = User.current
       @cert.imported_at = Time.now
 
       self.expand_pem
@@ -27,23 +35,34 @@ module ForemanCfssl
 
     def new
       @cert = Cert.new
+
+      # profiles for select
       config_path = SETTINGS[:foreman_cfssl][:config]
       config = JSON.parse(File.read(config_path))
       @profiles = config['signing']['profiles'].keys
+
+      # CA information
+      ca_pem = File.read(SETTINGS[:foreman_cfssl][:ca])
+      @ca_info = JSON.pretty_generate(extract_cert_info(ca_pem))
     end
 
     def create
+      # Added several attr_accessors in model to ease form processing
       @cert = Cert.new(params[:foreman_cfssl_cert].permit(:owner_email, :common_name, :profile, :hosts))
+
+      # Read ini configurations
       configs = SETTINGS[:foreman_cfssl]
       ca = configs[:ca]
       ca_key = configs[:ca_key]
       ca_config = configs[:config]
+      csr_template = configs[:csr_template]
 
-      # certificate signing request
-      csr = JSON.parse(File.read(configs[:csr_template]))
+      # fill in details to create certificate signing request config
+      csr = JSON.parse(File.read(csr_template))
       csr['CN'] = @cert.common_name
       csr['hosts'] = @cert.hosts.strip.split(%r{\s+})
 
+      # generate certificates
       stdin, stdout, stderr = Open3.popen3("cfssl gencert -config=#{ca_config} -ca=#{ca} -ca-key=#{ca_key} -profile=#{@cert.profile} -")
       stdin.puts JSON.dump(csr)
       stdin.close
@@ -51,6 +70,7 @@ module ForemanCfssl
       result = JSON.parse(stdout.gets(sep=nil))
       @cert.pem = result['cert']
       @cert.key = result['key']
+      @cert.user = User.current
 
       self.expand_pem
 
